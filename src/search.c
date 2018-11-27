6a3bd36a46d9250cf4be4459991d5ee78c0b8b2d
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2018-11-27 21:48:56 +0900

    update

--- src/search.c
+++ src/search.c
@@ -50,6 +50,8 @@ void search2018(unsigned char gx,unsigned char gy){
             wait_sw_on();
             wait_ms(1000);
 
+
+
             //自動的にコースの真ん中にセットしセンサーキャリブレーション
             turn(RIGHT90);                  //右向き右90度
             straight(70, -100, 0, 2, 0);	//後退ケツタッチ
@@ -58,6 +60,9 @@ void search2018(unsigned char gx,unsigned char gy){
             straight(70, -100, 0, 2, 0);    //後退ケツタッチ
             wait_ms(500);
             update_center_ref();            //センサーキャリブレーション
+
+
+
             set_wall_gain(5);//5            //制御ゲイン設定
 
             //マウス出発
@@ -67,7 +72,7 @@ void search2018(unsigned char gx,unsigned char gy){
             first_f = 0;//フラグリセット（スタート終わり）
         }
 
-        else if (goalx==mx && goaly==my){//ゴールまたはスタートに入った時の処理
+        else if (goalx==mx && goaly==my){//目的地（ゴールまたはスタート）に入った時の処理
             update_map(mx, my, dir);                                    //（１）区画の境目で壁の情報を更新
             LED_on(1);LED_on(2);LED_on(3);LED_on(4);
             straight(H_SECTION, speed, 0, acc, 1);//半歩前進
@@ -120,14 +125,31 @@ void search2018(unsigned char gx,unsigned char gy){
 		}
 
 		else {//普通の探索
-			update_map(mx, my, dir);                                    //（１）区画の境目で壁の情報を更新
-			uchar action = decide_action(mx, my, goalx, goaly, dir);    //（２）壁の情報に基づき移動方向を決定
-			move(action);                                               //（３）決定した行動を実行する
-			update_state(action, &mx ,&my, &dir);                       //（４）座標、方向の状態変数を更新する
+			update_map(mx, my, dir);                                            //（１）区画の境目で壁の情報を更新
+			uchar action = decide_action(mx, my, goalx, goaly, dir, S_MODE);    //（２）壁の情報に基づき移動方向を決定
+			move(action);                                                       //（３）決定した行動を実行する
+			update_state(action, &mx ,&my, &dir);                               //（４）座標、方向の状態変数を更新する
 		}
 	}
-
 }
+
+void make_shortest_run_command(uchar sx , uchar sy, uchar gx, uchar gy, uchar* command, uchar diag_flag ){
+    uchar x,y,dir;
+    x = sx;
+    y = sy;
+    dir = 0;
+    while (x==gx && y==gy){
+        *command = decide_action(x, y, gx, gy, dir, T_MODE);
+        update_state(*command, &x, &y, &dir );
+    }
+}
+
+void shortest_run(uchar sx, uchar sy, uchar gx, uchar gy, uchar* command){
+    
+    
+}
+
+
 #if 0
 void find_unsearched_block(uchar ox, uchar oy){
 	uchar x,y;
@@ -146,7 +168,7 @@ void find_unsearched_block(uchar ox, uchar oy){
 /* ======================================================================= */
 /* 等高線作成モジュール                                                    */
 /* ======================================================================= */
-void hight_map(uchar gx, uchar gy, uchar h,uchar mode)
+void hight_map(uchar gx, uchar gy, uchar h, uchar mode)
 {
 	// (gy,gx)ゴールにしたい座標
 	// h使っていない
@@ -156,9 +178,6 @@ void hight_map(uchar gx, uchar gy, uchar h,uchar mode)
 	uchar	x,y;
 	uchar	wdata;
 
-	//////////////////////////////////////////////////
-	//LED_on(1);LED_on(2);LED_on(3);LED_on(4);
-	//////////////////////////////////////////////////
 	//等高線マップを最候歩数255にリセット
 	for (y=0;y<16;y++){
 		for (x=0;x<16;x++){
@@ -204,11 +223,6 @@ void hight_map(uchar gx, uchar gy, uchar h,uchar mode)
 		pt0 = pt0+1;
 
 	}while(ct!=0);
-
-	/*-----------------------------------------------------*/
-	// LED_off(1);LED_off(2);LED_off(3);LED_off(4);
-	/*-----------------------------------------------------*/
-
 }
 
 
@@ -255,11 +269,11 @@ void update_map(uchar x, uchar y, uchar dir){
 	return;
 }
 
-short decide_action(uchar mx, uchar my, uchar gx, uchar gy, uchar dir){
+short decide_action(uchar mx, uchar my, uchar gx, uchar gy, uchar dir, uchar mode){
 
 	uchar front=255, left=255, right=255;
 
-	hight_map(gx, gy, 0, S_MODE);//等高線マップ作る
+	hight_map(gx, gy, 0, mode);//等高線マップ作る
 	front=255;left=255;right=255;
 
 	if ( dir == NORTH){//北向きだったら
@@ -522,218 +536,6 @@ void make_mapdata(void)
 
 
 #if 0
-//-------------------------------------------------------------------------
-//	探索関数
-//-------------------------------------------------------------------------
-void mouse_search( uchar y, uchar x, int spd )
-{
-	//探索が終わる条件
-	//1. ゴールした時に４分以上経過していた場合
-	//2. 一度ゴールしていてスタートに戻った時
-	//元プログラムは、選択できましたが原則スラ探にします。
-
-	uchar wall_data,vec,i,r_flag,l_flag,s_flag,tmp;
-
-	recovery();   //けつかっちん
-	mode6(1);     //Sensor Check
-	run_flag=1;
-	control_mode=1;
-	goal_flag = 0;
-	sura_flag=0;
-
-	while( 1 ){
-		// １つのループは区間中心から次の区間中心まで
-		// 最初に半区画直進
-
-		rdir = 0; ldir = 0;           // 回転方向を直進
-		STEP = 0;                     // 距離カウンタリセット
-		speed = spd;                  // 速度設定(これで自動的にマウスが移動開始)
-		control_mode = 1;             // 姿勢制御ON
-
-		if(sura_flag!=1){
-			while( STEP < GO_STEP / 2 )
-				if(SW_EXEC==SW_ON)return;
-		}  // 半区間進む
-		else STEP = GO_STEP/2;//スラロームフラグは
-
-		place_update();               // 現在座標更新
-		// 柱まで進んだら
-		wall_data = get_wall_data();  // 壁情報取得
-
-		if(map[my][mx]<0xf0)          // マッピングしていない区画なら
-			map_save(wall_data);        // マップを更新
-
-		LED_wall(wall_data);
-		wall_data|=symu_wall();
-
-		sura_flag=0;
-
-		if(mx==x&&my==y&&goal_flag==0)                          //初めてゴールした
-		{
-			while( STEP < GO_STEP - ( speed_now - 1 ) * 2 );
-			speed=1;
-			while( STEP < GO_STEP )if(SW_EXEC==SW_ON)return;
-			goal_flag=1;
-			com_turn(2);
-			countdown();
-			if(over_flag==1){
-				com_stop();
-				hight_map(0,0,0,T_MODE);
-				most_short(y,x,spd);
-				control_mode=1;
-				return;
-			}
-		}
-		else if(mx==0&&my==0)                                   //スタートにいる
-		{
-			if(goal_flag==1)
-			{
-				while( STEP < GO_STEP - ( speed_now - 1 ) * 2 );
-				speed = 1;
-				while( STEP < GO_STEP )if(SW_EXEC==SW_ON)return;
-				com_turn( 2 );
-				goal_flag=0;
-				run_flag=0;
-				countdown();
-				return;
-			}
-			else
-			{
-				while( STEP < GO_STEP - ( speed_now - 1 ) * 2 );
-				speed = 1;
-				while(STEP<GO_STEP);
-				com_turn(2);
-				map_initialize();
-				countdown();
-			}
-		}
-		else if(over_flag==1 && goal_flag ==1){                 //一度はゴールし時間オーバー
-			while( STEP < GO_STEP - ( speed_now - 1 ) * 2 );
-			speed =1;
-			while(STEP<GO_STEP);
-			com_stop();
-			control_mode=1;
-			hight_map(0,0,0,T_MODE);
-			wall_data=map_load();
-			if(wall_data!=3)com_turn(wall_data);
-			most_short(0,0,spd);
-			countdown();
-			return;
-		}
-
-		else if(spd>=200){                                      //スピードが200以上　スラローム探索
-			r_flag=l_flag=s_flag=0;
-			if((wall_data&0x08)==0x00)     // 左に壁がなかったら
-				l_flag=1;//com_sura(1);
-			if((wall_data&0x01)==0x00) // 前に壁がなかったら
-				s_flag=1;//while( STEP < GO_STEP );      // さらに半区間進む
-			if((wall_data&0x02)==0x00)// 右に壁がなかったら
-				r_flag=1;//com_sura(0);
-
-			if(l_flag==0&&r_flag==0&&s_flag==0){
-				vec=vector();
-				if(vec==2)
-				{
-					while( STEP < GO_STEP - ( speed_now - 1 ) * 2 );
-					speed = 1;                    // 残り半区間を減速しながら直進
-					while( STEP < GO_STEP );      // さらに半区間進む
-					com_turn( vec );
-				}
-				else if(vec<2)
-					com_sura(vec);
-				else
-					while(STEP<GO_STEP);
-			}
-
-			else if(r_flag&&l_flag&&s_flag){
-				tmp=wait_timer%3;
-				if(tmp==0)com_sura(0);
-				else if(tmp==1)com_sura(1);
-				else {while( STEP < GO_STEP );}
-			}
-			else if(l_flag&&s_flag){
-				tmp=wait_timer%2;
-				if(tmp==0)com_sura(1);
-				else {while( STEP < GO_STEP );}
-			}
-			else if(r_flag&&s_flag){
-				tmp=wait_timer%2;
-				if(tmp==0)com_sura(0);
-				else {while( STEP < GO_STEP );}
-			}
-			else if(r_flag&&l_flag){
-				tmp=wait_timer%2;
-				if(tmp==0)com_sura(0);
-				else if(tmp==1)com_sura(1);
-			}
-			else if(l_flag){
-				com_sura(1);
-			}
-			else if(r_flag){
-				com_sura(0);
-			}
-			else {while( STEP < GO_STEP );}
-
-
-		}
-
-		/*
-    else if(spd>=200){
-      if((wall_data&0x08)==0x00)     // 左に壁がなかったら
-        com_sura(1);
-      else if((wall_data&0x01)==0x00) // 前に壁がなかったら
-        while( STEP < GO_STEP );      // さらに半区間進む
-      else if((wall_data&0x02)==0x00)// 右に壁がなかったら
-        com_sura(0);
-      else{
-        vec=vector();
-        if(vec==2)
-        {
-          while( STEP < GO_STEP - ( speed_now - 1 ) * 2 );
-          speed = 1;                    // 残り半区間を減速しながら直進
-          while( STEP < GO_STEP );      // さらに半区間進む
-          com_turn( vec );
-        }
-        else if(vec<2)
-          com_sura(vec); 
-        else
-          while(STEP<GO_STEP);
-      }
-    }
-		 */
-
-		else{                                                                   //極地旋回モードで探索
-			if((wall_data&0x08)==0x00){     // 左に壁がなかったら
-				while( STEP < GO_STEP - ( speed_now - 1 ) * 2 );
-				speed = 1;                    // 残り半区間を減速しながら直進
-				while( STEP < GO_STEP )if(SW_EXEC==SW_ON)return;      // さらに半区間進む
-				com_turn( 1 );                // 左90度旋回
-			}
-			else if((wall_data&0x01)==0x00) // 前に壁がなかったら
-				while( STEP < GO_STEP );      // さらに半区間進む
-			else if((wall_data&0x02)==0x00){// 右に壁がなかったら
-				while( STEP < GO_STEP - ( speed_now - 1 ) * 2 );
-				speed = 1;                    // 残り半区間を減速しながら直進
-				while( STEP < GO_STEP );      // さらに半区間進む
-				com_turn( 0 );                // 右90度旋回
-			}
-			else{
-				vec=vector();
-				if(vec<=2)
-				{
-					while( STEP < GO_STEP - ( speed_now - 1 ) * 2 );
-					speed = 1;                    // 残り半区間を減速しながら直進
-					while( STEP < GO_STEP );      // さらに半区間進む
-					com_turn( vec );
-				}
-				else
-					while(STEP<GO_STEP);
-			}
-		}
-	}
-}
-
-
 //------------------------------------------------------------------------
 //最短
 //------------------------------------------------------------------------
